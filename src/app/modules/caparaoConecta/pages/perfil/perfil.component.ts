32c02a2f23080a544e14747ce88d56e42e7f6601
LabMaker Ifes Alegre <labmaker@MacBook-Air-de-LabMaker.local>
2025-05-05 10:27:24 -0300

    condicional no dialog de editar perfil para mudar entre empresa e candidato

--- src/app/modules/caparaoConecta/pages/perfil/perfil.component.ts
+++ src/app/modules/caparaoConecta/pages/perfil/perfil.component.ts
@@ -37,7 +37,7 @@ export class PerfilComponent implements OnInit {
   cursos: any[] = [];
   id_tipo_usuario: string = '';
 
-  idUsuario = 3;
+  idUsuario = 4;
   // Substitua pelo ID do usuário desejado
   // Substitua pelo ID do tipo de usuário desejado
 
