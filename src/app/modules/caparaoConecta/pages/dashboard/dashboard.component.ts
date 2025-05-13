227de801dbefcde57bee08c443bb6bd34da20e26
unknown <conecta@ALE-LABMAKER-20.cefetes.br>
2025-05-13 11:10:32 -0300

    grafico feito na dashboard...

--- src/app/modules/caparaoConecta/pages/dashboard/dashboard.component.ts
+++ src/app/modules/caparaoConecta/pages/dashboard/dashboard.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { CabecalhoComponent } from "../../components/cabecalho/cabecalho.component";
 import { DashboardCardComponent } from "./dashboard-card/dashboard-card.component";
 import { TabelaComponent } from './dashboard-tabela/dashboard-tabela.component';
+import { GraficoComponent } from "../../components/grafico/grafico.component";
+import { FooterComponent } from "../../components/footer/footer.component";
 
 @Component({
   selector: 'app-dashboard',
-  imports: [CabecalhoComponent, DashboardCardComponent, TabelaComponent],
+  imports: [CabecalhoComponent, DashboardCardComponent, TabelaComponent, GraficoComponent, FooterComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
