f7c0c1c956b986553e8cf24854172e5a52d7fb74
LabMaker Ifes Alegre <labmaker@MacBook-Air-de-LabMaker.local>
2025-11-18 11:35:27 -0300

    mudando de novo a api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   ibgeUrl: 'https://servicodados.ibge.gov.br/api/v1/localidades/estados/',
-  apiAuth: 'https://control.dev.br',
+  apiAuth: 'https://api-conecta.control.dev.br',
   apiCharts: 'https://www.gstatic.com/charts/loader.js',
 };
